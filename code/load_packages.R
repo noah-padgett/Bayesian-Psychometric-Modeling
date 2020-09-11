14fc61bec6395ed0c5c489383fea260b9788818b
noah-padgett <noah_padgett1@baylor.edu>
2020-09-11 11:55:51 -0500

    initial commit

---
+++ code/load_packages.R
@@ -0,0 +1,33 @@
+# ============================================= #
+# script: load_packages.R
+# Project: Code Guide BPM
+# Author(s): R.N. Padgett
+# ============================================= #
+# Data Created: 2020-09-01
+# Date Modified: 2020-09-04
+# By: R. Noah Padgett
+# ============================================= #
+# Stems from Padgett's Independent Study
+# ============================================= #
+# Purpose:
+# This R script is for loading all necessary
+#   R packages
+#
+# No output - just loading packages into the
+#   environment
+# ============================================= #
+# Set up directory and libraries
+# rm(list=ls())
+# list of packages
+packages <- c("patchwork", "tidyr", "dplyr", "ggplot2",
+              "R2jags","R2WinBUGS", 
+              "rstan", "brms", "bayesplot",
+              "kableExtra",
+              "dagitty", "ggdag", "ggraph", "cowplot")
+new.packages <- packages[!(packages %in% installed.packages()[,"Package"])]
+if(length(new.packages)) install.packages(new.packages)
+# Load packages
+lapply(packages, library, character.only = TRUE)
+
+w.d <- getwd()
+
---
+++ code/pdf2png.R
@@ -0,0 +1,28 @@
+# pdf to png
+
+library(pdftools)
+w.d <- getwd()
+
+files.all <- list.files(paste0(w.d, '/dag/'))
+
+files <- paste0(paste0(w.d, '/dag/'), grep(".pdf", files.all, value=T))
+files.out <- paste0(unlist(strsplit(files, ".pdf")), ".png")
+dpi <- rep(400,length(files))
+for(i in 1:length(files)){
+  pdf_convert(files[i], format = "png", dpi = dpi[i],
+              filenames = files.out[i])
+}
+
+
+
+# model specification diagrams
+files.all <- list.files(paste0(w.d, '/model-spec/'))
+
+files <- paste0(paste0(w.d, '/model-spec/'), grep(".pdf", files.all, value=T))
+files.out <- paste0(unlist(strsplit(files, ".pdf")), ".png")
+dpi <- rep(400,length(files))
+for(i in 1:length(files)){
+  pdf_convert(files[i], format = "png", dpi = dpi[i],
+              filenames = files.out[i])
+}
+
