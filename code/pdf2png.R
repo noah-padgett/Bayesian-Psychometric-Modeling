472863cabddc8b6a9f80b823587f181292c2198b
noah-padgett <noah_padgett1@baylor.edu>
2020-10-16 13:16:04 -0500

    included chp 9

--- code/pdf2png.R
+++ code/pdf2png.R
@@ -26,3 +26,16 @@ for(i in 1:length(files)){
               filenames = files.out[i])
 }
 
+
+
+# path diagrams
+files.all <- list.files(paste0(w.d, '/path-diagram/'))
+
+files <- paste0(paste0(w.d, '/path-diagram/'), grep(".pdf", files.all, value=T))
+files.out <- paste0(unlist(strsplit(files, ".pdf")), ".png")
+dpi <- rep(400,length(files))
+for(i in 1:length(files)){
+  pdf_convert(files[i], format = "png", dpi = dpi[i],
+              filenames = files.out[i])
+}
+
